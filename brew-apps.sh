5b3526431f4c3c74e51aaf0b0e6de349e0f84a9c
Bahadır UĞUR <baucomen@gmail.com>
2020-09-04 13:44:04 +0300

    Create brew-apps.sh

---
+++ brew-apps.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# install iTerm2 Terminal Emulator
+brew cask install iterm2
+
+# install sublime-text-3
+brew cask install sublime-text
+
+# install vscode
+brew cask install visual-studio-code
+
+# install firefox
+brew cask install firefox
+
+# install Docker
+brew cask install docker
+
+# install rectangle
+brew cask install rectangle
+
+# install android-file-transfer
+brew cask install android-file-transfer
+
+# install macdown
+brew cask install macdown
+
+# install macs-fan-control
+brew cask install macs-fan-control
+
+# install authy
+brew cask install authy
+
+# install surfshark
+brew cask install surfshark
